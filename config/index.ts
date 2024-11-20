d7289f45f1828a63f6f70453de60297be1128bd4
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2024-11-20 21:05:10 +0100

    chore(docs): Configure Swagger UI to use environment variables for scheme and host

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,10 @@
 const env = {
     PORT: parseInt(process.env.PORT!) || 3210,
     ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS,
+    SWAGGER: {
+        SCHEME: process.env.SCHEME || 'http',
+        HOST: `${process.env.HOST || 'localhost'}:${process.env.PORT || 3210}`,
+    },
 };
 
 export { env };
--- src/docs/index.ts
+++ src/docs/index.ts
@@ -2,11 +2,19 @@ import { Router } from 'express';
 import { readFileSync } from 'fs';
 import { serve, setup } from 'swagger-ui-express';
 import { parse } from 'yaml';
-const file = readFileSync('./src/docs/endpoints.yaml', 'utf8');
+import { env } from '../../config';
 
 const router = Router();
 
+const file = readFileSync('./src/docs/endpoints.yaml', 'utf8');
 const swaggerJsDocs = parse(file);
+
+swaggerJsDocs.servers.forEach((server: any) => {
+    server.variables.scheme.default =
+        env.SWAGGER.SCHEME || server.variables.scheme.default;
+    server.variables.host.default =
+        env.SWAGGER.HOST || server.variables.host.default;
+});
 
 router.use('/api-docs', serve, setup(swaggerJsDocs));
 
