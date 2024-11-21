898614f714a0ccb01a815b89f1e6eb60954d989d
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2024-11-21 04:31:43 +0100

    feat(env): Add example environment configuration for API and database settings

--- config/index.ts
+++ config/index.ts
@@ -1,9 +1,11 @@
 const env = {
-    PORT: parseInt(process.env.PORT!) || 3210,
+    PORT: parseInt(process.env.API_PORT!) || 3210,
     ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS,
     SWAGGER: {
-        SCHEME: process.env.SCHEME || 'http',
-        HOST: `${process.env.HOST || 'localhost'}:${process.env.PORT || 3210}`,
+        SCHEME: process.env.API_SCHEME || 'http',
+        HOST: `${process.env.API_HOST || 'localhost'}:${
+            process.env.API_PORT || 3210
+        }`,
     },
     QR_CODE: {
         width: parseInt(process.env.QR_CODE_WIDTH || '600'),
